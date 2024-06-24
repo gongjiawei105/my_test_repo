da5d36bfafe156f999e47a7ae731d08aa5237c5d
gongjiawei105 <gongjiawei105@gmail.com>
2024-06-24 16:21:02 -0400

    Create start.py

---
+++ start.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Create a line chart
+plt.plot(x, y)
+
+# Adding title and labels
+plt.title('Sample Line Graph')
+plt.xlabel('X Axis Label')
+plt.ylabel('Y Axis Label')
+
+# Show the plot
+plt.show()
